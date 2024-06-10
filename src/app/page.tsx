230a16e125360cbf767527bcbd90a9e582050fc6
Jordan Needham <jdncreations@hotmail.com>
2024-06-10 19:38:09 +0800

    change welcome msg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1> Hello there. </h1>        
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1>Welcome to my website.</h1>
     </main>
-  )
+  );
 }
