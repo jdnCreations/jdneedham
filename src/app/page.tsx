9a8f3cd4acf64c7ea4f5d57e741ec79d0f9c679b
Jordan Needham <jdncreations@hotmail.com>
2024-08-01 07:35:13 +0000

    updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>Welcome to my website.</h1>
+      <h1>Bot Website for prophdawg.</h1>
+	<p>WELCOME</p>
     </main>
   );
 }
